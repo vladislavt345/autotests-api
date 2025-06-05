7de87caf3997ef56ef7ef837dd6d24de4a10ea99
vladislavt345 <vladislavt30515@gmail.com>
2025-06-06 01:36:38 +0300

    8.3 маркировки Pytest. практика работы с pytest маркировкам

--- autotests-api/tests/test_create_user.py
+++ autotests-api/tests/test_create_user.py
@@ -1,22 +1,23 @@
 from http import HTTPStatus
 
-from clients.users.public_users_client import get_public_users_client
+import pytest
+
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-# Импортируем функцию для проверки ответа создания юзера
 from tools.assertions.users import assert_create_user_response
 
 
-def test_create_user():
-    public_users_client = get_public_users_client()
-
+@pytest.mark.users
+@pytest.mark.regression
+def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
+    # Удалили инициализацию API клиента из теста
     request = CreateUserRequestSchema()
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
-    # Используем функцию для проверки ответа создания юзера
     assert_create_user_response(request, response_data)
 
     validate_json_schema(response.json(), response_data.model_json_schema())
