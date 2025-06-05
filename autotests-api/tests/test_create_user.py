acb64ad19b5bc65cd0975d97d0b57fb0e1b8a541
vladislavt345 <vladislavt30515@gmail.com>
2025-06-05 22:01:36 +0300

    8.2 написание API автотестов используя Pytest. практика написания API автотестов

---
+++ autotests-api/tests/test_create_user.py
@@ -0,0 +1,22 @@
+from http import HTTPStatus
+
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+# Импортируем функцию для проверки ответа создания юзера
+from tools.assertions.users import assert_create_user_response
+
+
+def test_create_user():
+    public_users_client = get_public_users_client()
+
+    request = CreateUserRequestSchema()
+    response = public_users_client.create_user_api(request)
+    response_data = CreateUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    # Используем функцию для проверки ответа создания юзера
+    assert_create_user_response(request, response_data)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
